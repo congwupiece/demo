9538a85118ef4876f94bfca931a6b1c2abf2ccda
wangcongwu <wangcongwu@bytedance.com>
2015-12-01 13:32:32 +0800

    add page
    Change-Id: I0ce2be255a2874e312f93ecca0c74166a05017ff

--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,9 @@
-$("#l2").on("touchstart", function() {
+$("#l2").on("touchstart", function(e) {
     $(this).css("opacity", "0.5");
+    var x = e.originalEvent.touches[0].clientX;
+    var y = e.originalEvent.touches[0].clientY;
+    window.list = document.elementsFromPoint(x, y);
+    window.lis = document.elementFromPoint(x, y);
 });
 $("#l1").on("touchstart", function() {
     $(this).css("opacity", "0.5");
